e4a7309ca60e33aa05647021f89777debe2d275d
unknown <mistourt@gmail.com>
2024-06-21 18:07:38 -0500

    succesfully downloading and java and referencing java

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 export PROJECT_NAME="java-template"
 export openJdk_ver=22.0.1
 export op_sys=windows-x64
 export destination="$HOME/.configuration-dependencies/$PROJECT_NAME"
-destination_dep="$destination/OpenJDK-$openJdk_ver-$op_sys"
+export destination_dep="$destination/jdk-$openJdk_ver"
