8896729e04b01a6651a06c6196265e726cdbf60e
unknown <mistourt@gmail.com>
2024-06-23 12:28:06 -0500

    JAVA_HOME is not exported correctly within the bash ecosystem and gradle is now getting downloaded and tested

--- scripts/configure_Gradle.sh
+++ scripts/configure_Gradle.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-exportJdk()
+exportGradle()
 {
-	# Set JAVA_HOME temporarily
-	echo "JAVA_HOME being set to $destination_dep"
-	export JAVA_HOME="$destination_dep"
+	# Set GRADLE_HOME temporarily
+	echo "GRADLE_HOME being set to $gradle_destination"
+	export GRADLE_HOME="$gradle_destination"
 
-	# Add Java bin directory to PATH
-	export PATH="$JAVA_HOME/bin:$PATH"
+	# Add Gradle bin directory to PATH
+	export PATH="$GRADLE_HOME/bin:$PATH"
 }
 
-testDownload()
+testGradleDownload()
 {
 	#Testing the download
 	echo "Testing the download."
 	echo ""
 	echo ""
-	exportJdk
-	java -version
+	exportGradle
+	gradle -v
 	echo ""
 	echo ""
 	echo "Finished Testing the download."
 }
 
-if [ -d "$destination_dep" ]; then
-	echo "OpenJDK $openJdk_ver-$op_sys already exists."
-	testDownload
+if [ -d "$gradle_destination" ]; then
+	echo "Gradle $gradle_ver already exists."
+	testGradleDownload
 else
-	echo "Downloading OpenJDK v$openJdk_ver"
+	echo "Downloading Gradle v$gradle_ver"
 	# Define the URL of the OpenJDK installer
-	url="https://download.java.net/java/GA/jdk22.0.1/c7ec1332f7bb44aeba2eb341ae18aca4/8/GPL/openjdk-22.0.1_windows-x64_bin.zip"
+	url="https://github.com/gradle/gradle-distributions/releases/download/v8.8.0/gradle-8.8-bin.zip"
 
 	# Define the temporary file to store the downloaded installer
 	zip_file=$(mktemp)
@@ -47,13 +47,13 @@ else
 	fi
 	echo "Extracting the ZIP archive to the aforementioned destination directory."
 	unzip -q $zip_file -d "$destination"
-	echo "OpenJDK ZIP archive extracted to $destination."
+	echo "Gradle ZIP archive extracted to $destination."
 
 	# Clean up the ZIP file
 	echo "Cleaning up the downloaded ZIP file" 
 	rm $zip_file
 
-	testDownload
+	testGradleDownload
 	
 fi
 
--- scripts/constants.sh
+++ scripts/constants.sh
@@ -5,3 +5,5 @@ export openJdk_ver=22.0.1
 export op_sys=windows-x64
 export destination="$HOME/.configuration-dependencies/$PROJECT_NAME"
 export destination_dep="$destination/jdk-$openJdk_ver"
+export gradle_ver=8.8
+export gradle_destination="$destination/gradle-$gradle_ver"
